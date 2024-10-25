561bcc6097c2b7bebb132fe9675a16dc14ad9059
Kenny Lee <144768000+KennyHacrt@users.noreply.github.com>
2024-10-25 12:46:18 +0800

    Update Bracket balance LV.2.java

--- Bracket balance LV.2.java
+++ Bracket balance LV.2.java
@@ -45,3 +45,59 @@ public class bracekt {
 
     }
 }
+/// using stack ver
+import java.util.Stack;
+public class Main {
+    public static void main(String[] args) {
+        int test[]={9,3,5,2,7,6,8,0,1,4};
+        char test2[]={'(', '{', '[', '}', ')'};
+//        bubble_recursive(test);
+//        for (int i:test) System.out.println(i);
+        System.out.println(balancing(test2));
+    }
+    //Input a = [1,2,3,4,5,6,7],p = 2
+    //Output [3,4,5,6,7,1,2]
+    //Input a = [3,4,5],p = 4
+    // Output [4,5,3]
+    public static void bubble_recursive(int test[]){
+    }
+    public static boolean balancing(char test2[]){
+        Stack <Character> stack= new Stack <Character>();
+        int count=0;
+        int len =test2.length;
+        if (len==0 || len%2==1) return false;
+        for(int i=0;i<test2.length;i++){
+            if (test2[i]=='(') {
+                stack.push('(');
+                count++;
+            }
+            else if (test2[i]=='[') {
+                stack.push('[');
+                count++;
+            }
+            else if (test2[i]=='{') {
+                stack.push('{');
+                count++;
+            }
+            else if (count!=0) {
+                if (stack.peek() == '(' && test2[i] == ')') {
+                    stack.pop();
+                    count--;
+                } else if (stack.peek() == '[' && test2[i] == ']') {
+                    stack.pop();
+                    count--;
+                } else if (stack.peek() == '{' && test2[i] == '}') {
+                    stack.pop();
+                    count--;
+                }
+                else return false;
+            }
+            else return false;
+
+        }
+        if (stack.isEmpty()) return true;
+        return false;
+    }
+
+}
+
